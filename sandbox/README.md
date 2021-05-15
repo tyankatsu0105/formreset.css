7a73d09d0ef0a0846982745a2d97b4eb339dae32
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-05-16 01:28:08 +0900

    chore: add website

--- sandbox/README.md
+++
@@ -1,7 +0,0 @@
-Test style at here.
-
-```bash
-npm run sandbox
-```
-
-http://localhost:5000/sandbox/
---
+++ website/stories/button/button.stories.js
@@ -0,0 +1,5 @@
+export default {
+  title: "button/button",
+};
+
+export const Show = () => `<button type="button">button</button>`;
